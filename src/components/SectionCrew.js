d0a11537b8a0d5400f2b5fc078f2bc18014d5e33
Stackbit Code Editor <support@stackbit.com>
2021-02-16 13:00:06 +0000

    SectionCrew.js: changed

--- src/components/SectionCrew.js
+++ src/components/SectionCrew.js
@@ -7,7 +7,7 @@ export default class SectionReviews extends React.Component {
     render() {
         let section = _.get(this.props, 'section', null);
         return (
-            <section id={_.get(section, 'section_id', null)} className={'block reviews-block bg-' + _.get(section, 'background', null) + ' outer'}>
+            <section id={_.get(section, 'section_id', null)} className={'block crew-block bg-' + _.get(section, 'background', null) + ' outer'}>
               <div className="block-header inner-small">
                 {_.get(section, 'title', null) && (
                 <h2 className="block-title">{_.get(section, 'title', null)}</h2>

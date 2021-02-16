72a863852d2dfddf4c17b63e549864554092bd10
Stackbit Code Editor <support@stackbit.com>
2021-02-16 09:45:34 +0000

    SectionCrew.js: changed

--- src/components/SectionCrew.js
+++ src/components/SectionCrew.js
@@ -32,7 +32,7 @@ export default class SectionReviews extends React.Component {
                         <cite className="review-author">{_.get(review, 'author', null)}</cite>
                       </footer>
                     </div>
-                  </blockqh2uote>
+                  </h2>
                   ))}
                 </div>
               </div>
